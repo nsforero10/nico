bd766a0214ef948c8054c029a59b18098581b855
Nicolas <nicolas@kiwicampus.com>
2019-11-07 18:02:41 -0500

    Updates

---
+++ precache-manifest.2203ea0236a706253123851f4f0415ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72c8e5c812538a6cd326f83e8d96d42",
+    "url": "/nico.github.io/index.html"
+  },
+  {
+    "revision": "5214bc9df96fdb16ee26",
+    "url": "/nico.github.io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6bd90c57d7619423103c",
+    "url": "/nico.github.io/static/js/2.4293a90f.chunk.js"
+  },
+  {
+    "revision": "5214bc9df96fdb16ee26",
+    "url": "/nico.github.io/static/js/main.4b181ed2.chunk.js"
+  },
+  {
+    "revision": "2fac8593c707c66c5f64",
+    "url": "/nico.github.io/static/js/runtime-main.ecdd21af.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/nico.github.io/static/media/logo.25bf045c.svg"
+  }
+]);
